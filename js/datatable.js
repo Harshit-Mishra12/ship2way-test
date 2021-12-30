45abc1947f510ddec380628c5a6ed841f890dbf9
pallav <pallavbansal2@gmail.com>
2021-12-30 16:10:17 +0530

    changes

--- js/datatable.js
+++ js/datatable.js
@@ -49,3 +49,20 @@ $(document).ready( function () {
     //$('#cancelledTable_filter label input').attr("placeholder","Search Table");
 } );
 
+$(document).ready( function () {
+    var extensions = {
+        "sFilter": "dataTables_filter search-box",
+    }
+    // Used when bJQueryUI is false
+    $.extend($.fn.dataTableExt.oStdClasses, extensions);
+    // Used when bJQueryUI is true
+    $.extend($.fn.dataTableExt.oJUIClasses, extensions);
+    $('#holdTable').dataTable({
+        "bInfo": false, //Dont display info e.g. "Showing 1 to 4 of 4 entries"
+         "paging": false,//Dont want paging                
+         "bPaginate": false,//Dont want paging   
+         language: { search: '', searchPlaceholder: "Search Table" },
+    });
+    //$('#cancelledTable_filter label input').attr("placeholder","Search Table");
+} );
+
